3f22bda61b6f3ac2fcf8e4b6fd4e2f4a40f9f061
kb18519142009 <847584597@qq.com>
2018-04-12 14:57:40 +0800

    添加注释、修改bug等

--- app/src/main/java/com/example/likedouyin/view/HorizontalViewPager.java
+++ app/src/main/java/com/example/likedouyin/view/HorizontalViewPager.java
@@ -25,12 +25,13 @@ public class HorizontalViewPager extends ViewPager {
     }
 
     private void init() {
-        setOverScrollMode(OVER_SCROLL_NEVER);
+        setOverScrollMode(OVER_SCROLL_NEVER); //设置去掉滑到最左或最右时的滑动效果
     }
 
     private class XScrollDetector extends GestureDetector.SimpleOnGestureListener {
         @Override
         public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+            // 横向滑动大于纵向滑动距离的2.5倍时返回true，更好的解决某些事件冲突问题
             return Math.abs(distanceX) > Math.abs(distanceY) * 2.5;
         }
     }
@@ -39,7 +40,7 @@ public class HorizontalViewPager extends ViewPager {
     public boolean onInterceptTouchEvent(MotionEvent ev) {
         if (xScrollDetector.onTouchEvent(ev)) {
             super.onInterceptTouchEvent(ev);
-            return true;
+            return true; //如果是横向滑动事件，就拦截事件，不再向下传递，自己消费事件
         }
 
         return super.onInterceptTouchEvent(ev);
